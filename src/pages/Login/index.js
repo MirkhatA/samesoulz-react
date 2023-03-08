e078f5e0c18f4ae46d0f5f62d54fe44a0c3ab5f3
mirkek <asen.mirkhat@bk.ru>
2023-03-08 11:41:20 +0600

    sign up link

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,13 +2,18 @@ import "./index.css";
 
 import LoginInput from "../../components/ui/LoginInput";
 import AuthSubmitButton from "../../components/ui/AuthSubmitButton";
+import { Link } from "react-router-dom";
 
 export default function Login() {
     return (
         <div className="flex h-screen">
             <div className="relative max-w-sm m-auto">
                 <div className="relative flex flex-col items-center">
-                    <img src="/icons/logo_img.png" className="smallIcon" alt="logo_img" />
+                    <img
+                        src="/icons/logo_img.png"
+                        className="smallIcon"
+                        alt="logo_img"
+                    />
                 </div>
 
                 <div className="login-form">
@@ -19,6 +24,12 @@ export default function Login() {
                         <LoginInput>Password...</LoginInput>
                         <AuthSubmitButton>Submit</AuthSubmitButton>
                     </form>
+                </div>
+
+                <div className="relative flex flex-col items-center">
+                    <small className="already-have-account">
+                        <Link to="/registration">Sign up</Link>
+                    </small>
                 </div>
             </div>
         </div>
